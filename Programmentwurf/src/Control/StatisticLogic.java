f0af0f7dac1ca84cada8d897422e7eaa2ecf0ccb
jasminsteidl <mine.maus99@freenet.de>
2024-06-20 13:50:15 +0200

    statistic screen

---
+++ Programmentwurf/src/Control/StatisticLogic.java
@@ -0,0 +1,13 @@
+package Control;
+
+import Model.Media;
+import View.StatistikGui;
+import java.util.List;
+
+public class StatisticLogic {
+    private StatistikGui statistikGui = new StatistikGui();
+    public void openFavScreen() {
+        statistikGui.statisticScreen();
+
+    }
+}
---
+++ Programmentwurf/src/View/StatistikGui.java
@@ -0,0 +1,34 @@
+package View;
+
+import Control.StatisticLogic;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StatistikGui extends JFrame {
+    static JFrame f;
+
+    public static void statisticScreen() {
+        f = new JFrame("Statistic Screen");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        JButton openStatistic = new JButton("Statistik öffnen");
+
+
+        f.setSize(600, 400);
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        f.setVisible(true);
+
+        openStatistic.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StatisticLogic statisticLogic = new StatisticLogic();
+                statisticLogic.openFavScreen();
+            }
+        });
+    }
+}
